8f4e0f569d6b153ceb066524a951e8f9b447c3de
rossellamaruccia <126196298+rossellamaruccia@users.noreply.github.com>
2025-11-04 16:53:58 +0100

    second ex not finished

--- scripts.js
+++ scripts.js
@@ -5,19 +5,50 @@ const savedName = document.getElementById("savedName")
 const saveButton = document.getElementById("save")
 const deleteButton = document.getElementById("delete")
 
-const KEY = 'memory'
+const KEY = "memory"
 
 saveName = function (e) {
   e.preventDefault()
   const name = nameInput.value
-    localStorage.setItem(KEY, name)
-    savedName.innerHTML = `<h3>You have inserted the name: ${name}</h3>`
-  // uno salverà il valore in localStorage, uno rimuoverà il valore precedentemente salvato (se presente).
+  localStorage.setItem(KEY, name)
+  savedName.innerHTML = `<h3>You have inserted the name: ${name}</h3>`
+
   return form.reset()
 }
 
 deleteName = function (e) {
-    e.preventDefault()
-    savedName.innerHTML = `<h3>You haven't inserted a name yet</h3>`
+  e.preventDefault()
+  savedName.innerHTML = `<h3>You haven't inserted a name yet</h3>`
   return localStorage.removeItem(KEY)
+}
+
+// Crea un contatore che tenga conto del tempo che passa, utilizzando sessionStorage.
+// Aggiornando la pagina il valore prosegue, chiudendo la pagina - ovviamente - ricomincia.
+// Il valore del contatore deve aggiornarsi ad ogni secondo.
+const display = document.getElementById("display")
+let seconds = 0
+const TIME = "memory"
+
+let startTimer = function (e) {
+  e.preventDefault()
+  setInterval(
+    function () {
+      display.innerHTML = seconds++
+      let value = seconds
+      sessionStorage.setItem(TIME, value)
+    },
+    1000
+  )
+}
+
+let pauseTimer = function (e) {
+  e.preventDefault()
+    let timeValue = sessionStorage.getItem('memory', value)
+    display.innerHTML = timeValue
+}
+
+let resetTimer = (e) => {
+  e.preventDefault()
+  display.innerHTML = 0
+  sessionStorage.clear()
 }
